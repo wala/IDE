4c6903337a1b7d85282786f4ee3156c45db2b9ff
Julian Dolby <juliandolby@gmail.com>
2018-03-19 19:13:03 -0400

    initial commit of LSP support

---
+++ com.ibm.wala.cast.lsp/source/com/ibm/wala/cast/lsp/WALAServer.java
@@ -0,0 +1,299 @@
+package com.ibm.wala.cast.lsp;
+
+import java.net.URL;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+import java.util.function.IntSupplier;
+
+import org.eclipse.lsp4j.CodeActionParams;
+import org.eclipse.lsp4j.CodeLens;
+import org.eclipse.lsp4j.CodeLensParams;
+import org.eclipse.lsp4j.Command;
+import org.eclipse.lsp4j.CompletionItem;
+import org.eclipse.lsp4j.CompletionList;
+import org.eclipse.lsp4j.DidChangeTextDocumentParams;
+import org.eclipse.lsp4j.DidCloseTextDocumentParams;
+import org.eclipse.lsp4j.DidOpenTextDocumentParams;
+import org.eclipse.lsp4j.DidSaveTextDocumentParams;
+import org.eclipse.lsp4j.DocumentFormattingParams;
+import org.eclipse.lsp4j.DocumentHighlight;
+import org.eclipse.lsp4j.DocumentOnTypeFormattingParams;
+import org.eclipse.lsp4j.DocumentRangeFormattingParams;
+import org.eclipse.lsp4j.DocumentSymbolParams;
+import org.eclipse.lsp4j.Hover;
+import org.eclipse.lsp4j.InitializeParams;
+import org.eclipse.lsp4j.InitializeResult;
+import org.eclipse.lsp4j.Location;
+import org.eclipse.lsp4j.ReferenceParams;
+import org.eclipse.lsp4j.RenameParams;
+import org.eclipse.lsp4j.SignatureHelp;
+import org.eclipse.lsp4j.SymbolInformation;
+import org.eclipse.lsp4j.TextDocumentPositionParams;
+import org.eclipse.lsp4j.TextEdit;
+import org.eclipse.lsp4j.WorkspaceEdit;
+import org.eclipse.lsp4j.jsonrpc.messages.Either;
+import org.eclipse.lsp4j.services.LanguageClient;
+import org.eclipse.lsp4j.services.LanguageClientAware;
+import org.eclipse.lsp4j.services.LanguageServer;
+import org.eclipse.lsp4j.services.TextDocumentService;
+import org.eclipse.lsp4j.services.WorkspaceService;
+
+import com.ibm.wala.cast.loader.AstMethod;
+import com.ibm.wala.cast.tree.CAstSourcePositionMap.Position;
+import com.ibm.wala.classLoader.IMethod;
+import com.ibm.wala.ipa.callgraph.CGNode;
+import com.ibm.wala.ipa.callgraph.CallGraph;
+import com.ibm.wala.ipa.callgraph.propagation.HeapModel;
+import com.ibm.wala.ipa.callgraph.propagation.PointerKey;
+import com.ibm.wala.ssa.IR;
+import com.ibm.wala.ssa.SSAInstruction;
+import com.ibm.wala.util.collections.HashMapFactory;
+import com.ibm.wala.util.collections.HashSetFactory;
+
+public class WALAServer implements LanguageClientAware, LanguageServer {
+	private final Set<LanguageClient> clients = HashSetFactory.make();
+	private final Map<URL, NavigableMap<Position,PointerKey>> values = HashMapFactory.make();
+	
+	private final Set<Function<PointerKey,String>> analyses = HashSetFactory.make();
+	
+	private void ensureUrlValues(URL url) {
+		if (! values.containsKey(url)) {
+			values.put(url, new TreeMap<Position,PointerKey>(new Comparator<Position>() {
+				private int check(int v1, int v2, IntSupplier otherwise) {
+					if (v1 != v2) {
+						return v1 - v2;
+					} else {
+						return otherwise==null? 0: otherwise.getAsInt();
+					}
+				}
+				
+				@Override
+				public int compare(Position o1, Position o2) {
+					return 
+						check(o1.getFirstLine(), o2.getFirstLine(),
+							() -> { return check(o1.getFirstCol(), o2.getFirstCol(),
+								() -> { return check(o1.getLastLine(), o2.getLastLine(),
+									() -> { return check(o1.getLastCol(), o2.getLastCol(), null); }); }); });
+				} 
+			}));
+		}
+	}
+	
+	private void add(Position p, PointerKey v) {
+		URL url = p.getURL();
+		ensureUrlValues(url);
+		values.get(url).put(p, v);
+	}
+	
+	public void add(Function<PointerKey,String> analysis) {
+		analyses.add(analysis);
+	}
+	
+	public PointerKey getValue(Position p) {
+		NavigableMap<Position, PointerKey> m = values.get(p.getURL());
+		if (m.containsKey(p)) {
+			return m.get(p);
+		} else {
+			return m.ceilingEntry(p).getValue();
+		}
+	}
+	
+	public WALAServer(CallGraph CG, HeapModel H) {
+		for(CGNode n : CG) {
+			IMethod M = n.getMethod();
+			IR ir = n.getIR();
+			for(SSAInstruction inst : ir.getInstructions()) {
+				if (inst != null && inst.hasDef()) {
+					PointerKey v = H.getPointerKeyForLocal(n, inst.getDef());
+					if (M instanceof AstMethod) {
+						Position pos = ((AstMethod)M).debugInfo().getInstructionPosition(inst.iindex);
+						if (pos != null) {
+							add(pos, v);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	public CompletableFuture<InitializeResult> initialize(InitializeParams params) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CompletableFuture<Object> shutdown() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void exit() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public TextDocumentService getTextDocumentService() {
+		return new TextDocumentService() {
+
+			@Override
+			public CompletableFuture<Either<List<CompletionItem>, CompletionList>> completion(
+					TextDocumentPositionParams position) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public CompletableFuture<CompletionItem> resolveCompletionItem(CompletionItem unresolved) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public CompletableFuture<Hover> hover(TextDocumentPositionParams position) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public CompletableFuture<SignatureHelp> signatureHelp(TextDocumentPositionParams position) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public CompletableFuture<List<? extends Location>> definition(TextDocumentPositionParams position) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public CompletableFuture<List<? extends Location>> references(ReferenceParams params) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public CompletableFuture<List<? extends DocumentHighlight>> documentHighlight(
+					TextDocumentPositionParams position) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public CompletableFuture<List<? extends SymbolInformation>> documentSymbol(DocumentSymbolParams params) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public CompletableFuture<List<? extends Command>> codeAction(CodeActionParams params) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public CompletableFuture<List<? extends CodeLens>> codeLens(CodeLensParams params) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public CompletableFuture<CodeLens> resolveCodeLens(CodeLens unresolved) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public CompletableFuture<List<? extends TextEdit>> formatting(DocumentFormattingParams params) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public CompletableFuture<List<? extends TextEdit>> rangeFormatting(DocumentRangeFormattingParams params) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public CompletableFuture<List<? extends TextEdit>> onTypeFormatting(DocumentOnTypeFormattingParams params) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public CompletableFuture<WorkspaceEdit> rename(RenameParams params) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public void didOpen(DidOpenTextDocumentParams params) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void didChange(DidChangeTextDocumentParams params) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void didClose(DidCloseTextDocumentParams params) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void didSave(DidSaveTextDocumentParams params) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		};
+	}
+
+	@Override
+	public WorkspaceService getWorkspaceService() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void connect(LanguageClient client) {
+		clients.add(client);
+	}
+
+	public String toString() {
+		StringBuffer sb = new StringBuffer("WALA Server:\n");
+		for(URL script : values.keySet()) {
+			for(Map.Entry<Position, PointerKey> v : values.get(script).entrySet()) {
+				sb.append(v.getKey() + ":" + v.getValue());
+				for(Function<PointerKey,String> a : analyses) {
+					String s = a.apply(v.getValue());
+					if (s != null) {
+						sb.append(" :" + s);
+					}
+				}
+				sb.append("\n");
+			}
+		}
+		return sb.toString();
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
