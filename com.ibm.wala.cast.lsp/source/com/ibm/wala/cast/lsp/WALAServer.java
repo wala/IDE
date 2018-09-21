c3f9cab2fb69fa276f86aadffa054a2341169349
Julian Dolby <juliandolby@gmail.com>
2018-09-20 20:48:17 -0400

    flush

--- com.ibm.wala.cast.lsp/source/com/ibm/wala/cast/lsp/WALAServer.java
+++ com.ibm.wala.cast.lsp/source/com/ibm/wala/cast/lsp/WALAServer.java
@@ -16,6 +16,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.io.Reader;
 import java.net.MalformedURLException;
@@ -1492,7 +1493,7 @@ public class WALAServer implements LanguageClientAware, LanguageServer {
 			OutputStream out) throws IOException {
 		WALAServer server = new WALAServer(languages);
 		Launcher<LanguageClient> launcher = 
-			LSPLauncher.createServerLauncher(server, logStream(in, "wala.lsp.in"), logStream(out, "wala.lsp.out"), true, new PrintWriter(System.err));
+			LSPLauncher.createServerLauncher(server, logStream(in, "wala.lsp.in"), logStream(new PrintStream(out, true), "wala.lsp.out"), true, new PrintWriter(System.err));
 		server.connect(launcher.getRemoteProxy());
 		launcher.startListening();
 		return server;
